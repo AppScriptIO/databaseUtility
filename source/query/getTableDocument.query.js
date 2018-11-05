b24527009c3a89ea014d18d291d84eae8649a8e2
Dr. Safi <sfmissive@gmail.com>
2018-11-05 02:20:58 +0200

    ✒ Add assertion checks & change behavior of curried request for get doc

--- source/query/getTableDocument.query.js
+++ source/query/getTableDocument.query.js
@@ -5,6 +5,8 @@ import assert from "assert"
 // IMPORTANT: when false value (null, empty, undefined) is passed as key, the query will get all keys in db document.
 function getTableDocument(databaseName, tableName) {
     return async function getCondition(connection, key) {
+        assert(connection, 'Rethinkdb `connection` should be set.')
+        assert.strictEqual(Object.getPrototypeOf(connection).constructor.name, 'TcpConnection')
         let result;
         if(key) {
             result = await r
@@ -30,6 +32,8 @@ function getTableDocument(databaseName, tableName) {
 }
 
 async function getTableDocumentAllParams({ databaseName, tableName, connection, key }) {
+    assert(connection, 'Rethinkdb `connection` should be set.')
+    assert.strictEqual(Object.getPrototypeOf(connection).constructor.name, 'TcpConnection')
     let result;
     if(key) {
         result = await r
@@ -53,7 +57,7 @@ async function getTableDocumentAllParams({ databaseName, tableName, connection, 
     }
 }
 
-let requiredArgument = ['databaseName', 'tableName', 'connection', /* 'key' */ ] // commented out are optional
+let requiredArgument = ['databaseName', 'tableName', 'connection', 'key' ] // commented out are optional
 let curried = curryNamed(requiredArgument, getTableDocumentAllParams) // 
 
 export { getTableDocument as default, curried}
